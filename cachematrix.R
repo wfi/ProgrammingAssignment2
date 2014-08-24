b77fb8b766f5fddd99843d1205c85c7503186051
wfi <iba@westmont.edu>
2014-08-23 23:34:55 -0700

    added starting code to the makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -7,9 +7,23 @@
 
 ## makeCacheMatrix: create a matrix-like object that
 ## supports various getters and setters as well caching the inversion
-
+## Like the sample vector code, makeCacheMatrix returns a list of functions that
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse 
+## 4. get the value of the inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <- inverse
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
