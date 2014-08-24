2fce8a07a87a47237860c5b50c1afb0619ed5ab6
wfi <iba@westmont.edu>
2014-08-23 23:25:22 -0700

    added some comments to the given file

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Code for Programming Assignment 2 in 
+## Coursera R Programming course
 
-## Write a short comment describing this function
+## These functions are supposed to manage a matrix and allow
+## the inversion of the matrix to be cached and subsequently
+## retrieved (if the matrix has not been modified since the cache)
+
+## makeCacheMatrix: create a matrix-like object that
+## supports various getters and setters as well caching the inversion
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: given a matrix, return the inverse utilizing the
+## cache if it exists and computing (and caching) if it doesn't exist
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
